c087cd7352b6d46ea0c15bd32e516e18922b13ff
bschilder <brian.schilder@mssm.edu>
2020-01-07 15:27:25 -0500

    Initialize repo

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # ggLocusZoom
-Create LocusZoom plots (http://locuszoom.org) in R.
+ 
+
+## Why?  
+
+[LocusZoom](http://locuszoom.org) is a very useful and commonly used tool for visualizing genomic signals in GWAS/QTL data. But when you're making many plots for many loci, it makes sense to do this programmatically. 
+There is a [standalone version of LocusZoom](https://github.com/statgen/locuszoom-standalone) for R and Python, unfortunately but it requires downloading 23GB of data to get all of the databases it relies on. Even if you supply your own data (summary stats, LD, etc.), it's not particularly user friendly. Nor can you customize it further once you have it.
+
+So I made a user-friendly version of it in R that allows you to customize it as a ggplot-like object. Right now you need to supply you own data, but I'd like to add API access to datasets eventually.
+
+**Note**: LocusZoom now offers the ability to upload all your sum stats at once and make plots for each locus, which helps reduce the redundancy of making each plot individually.
+
+
+- Required columns in sumstats data (one SNP per row):
+  + `SNP` :  rsid (e.g. rs4698412)
+  + `P` : uncorrected p-value from GWAS (e.g. 2.058e-28)
+  + `CHR` : chromosome (e.g. 4)
+  + `POS` :  genomic position in terms of bp (e.g. 15737348)
+  
+<hr>
+
+## How?  
+
+```
+gglz <- ggLocusZoom(
+                    # Specify where the summary stats are.
+                    sumstats_path="./example_data/BST1_Nalls23andMe_2019_subset.txt", 
+                    # Specify where the pre-computed LD matrix is.
+                    LD_path="../example_data/UKB_LD.RDS",
+                    # Is the LD matrix in units of r? (as opposed to r2)
+                    LD_units="r",
+                    # Show the plot when it's ready?
+                    show_plot=T,
+                    # Save the plot? (=F if you don't want to)
+                    save_plot="./ggLocusZoom.png",
+                    # Saved plot height.
+                    height=5, 
+                    # Saved plot width.
+                    width=10,
+                    # Optional plot title.
+                    title="",
+                    # Draw a vertical line at the position of the lead SNP.
+                    leadSNP.line=T,
+                    # Plot LD with the leda SNP as a categorical variable (very low, low, medium, high, very high) or a continuous variable (0-1)
+                    categorical_r2=T)
+```
+
+### 1. 
+  
+<hr><hr>
+
+## Who?  
+
+Brian M. Schilder
+[Raj Lab](www.rajlab.org)
+Department of Neuroscience
+Department of Genetics & Genomic Sciences
+Ronald M. Loeb Center for Alzheimer's Disease
+Icahn School of Medicine at Mount Sinai
---
+++ ggLocusZoom.R
@@ -0,0 +1,117 @@
+
+
+ 
+ggLocusZoom <- function(sumstats_path="./example_data/BST1_Nalls23andMe_2019_subset.txt", 
+                        LD_path="../example_data/UKB_LD.RDS",
+                        LD_units="r",
+                        show_plot=T,
+                        save_plot="./ggLocusZoom.png",
+                        height=5, 
+                        width=10,
+                        title="",
+                        leadSNP.line=T,
+                        categorical_r2=T){
+  library(ggbio) # This package ALIGNS lots of different data types in tracks (like UCSC Genome Browser).
+  # See ggbio tutorial for more options: http://bioconductor.org/packages/release/bioc/vignettes/ggbio/inst/doc/ggbio.pdf
+  library(dplyr)
+  library(EnsDb.Hsapiens.v75) # BiocManager::install("EnsDb.Hsapiens.v75")
+  library(biovizBase)
+  # library(emojifont)
+  
+  # ------------- [1.] IMPORT DATA ------------- 
+  # Start with a dataframe containing your SNP-wise summary stats
+  dat <- data.table::fread(sumstats_path)
+  
+  
+  # ------------- [2. PREPARE DATA] ------------- 
+  # Identify the lead SNP from the GWAS
+  dat <- dat %>% mutate(leadSNP = ifelse(P==min(dat$P),T,F))
+  
+  # Get your LD 
+  if(endsWith(LD_path,".RDS")){
+    LD_matrix <- readRDS(LD_path)
+  } else {
+    LD_matrix <- data.table::fread(LD_path)
+  }
+ 
+  ## Get the r2 with your lead SNP
+  r2.dat <- LD_matrix[,subset(dat,leadSNP==T)$SNP] 
+  if(LD_units=="r") r2.dat <- r2.dat^2
+  r2.dat <- data.frame(SNP=names(r2.dat), r2=r2.dat)
+  ### Turn r2 into categories
+  r2.dat <- r2.dat %>% mutate(r2.group=cut(r2, breaks =  c(0,.2,.4,.6,.8,1), right=F, 
+                                 labels=c("very low","low","medium","high","very high")))
+  ## Merge the r2 with your original
+  dat <- merge(dat, r2.dat, by="SNP")  
+  
+  # Now, convert it to a GRanges object
+  dat <- dat %>% dplyr::mutate(SEQnames = paste0("chr",CHR))
+  gr.snp <- biovizBase::transformDfToGr(dat, seqnames = "CHR", start = "POS", end = "POS")
+  
+  lead.snp <- data.frame(subset(gr.snp, leadSNP))
+  
+  # ------------- [3.] CREATE TRACKS ------------- 
+  
+  # [3.1] Manhattan track 
+  ## Replace r2.group with r2 for continous LD
+  if(categorical_r2){
+    track.manhattan <- plotGrandLinear(obj=gr.snp, aes(y=-log10(P), color=r2.group, shape=leadSNP),
+                                       show.legend=T, alpha=.75) + 
+      scale_color_brewer(palette = "Spectral",direction = -1) + 
+      scale_shape_manual(values=c(16,18)) +
+      annotate(geom="text", x = lead.snp$POS, y = -log10(lead.snp$P)*1.05,label=lead.snp$SNP, size=3) +
+      theme_bw()
+  } else {
+    track.manhattan <- plotGrandLinear(obj=gr.snp, aes(y=-log10(P), color=r2, shape=leadSNP),
+                                       show.legend=T, alpha=.75) + 
+      scale_color_gradient(low="blue", high="red", limits = c(0,1)) + 
+      scale_shape_manual(values=c(16,18)) +
+      annotate(geom="text", x = lead.snp$POS, y = -log10(lead.snp$P)*1.05,label=lead.snp$SNP, size=3) +
+      theme_bw()
+  } 
+  # track.manhattan
+  
+  # [3.2] Manhattan track
+  # You can pull gene models from EnsDb.Hsapiens.v75. 
+  # But there's often way more than you can plot, so you have to filter them.
+  # Get the row of the leadSNP so you can focus on gene annotations around it.
+  lead.index <- which(gr.snp$leadSNP==T) 
+  lw <- 10 
+  track.genes <- autoplot(EnsDb.Hsapiens.v75::EnsDb.Hsapiens.v75, 
+                          # Have to limit (can only handle depth < 1000)
+                          which = gr.snp[(lead.index-lw):(lead.index+lw),], 
+                          names.expr = "gene_name",  
+                          aes(fill=gene_name, color=gene_name),
+                          show.legend=T)  +  
+    theme_bw()
+   
+  
+  
+  # [4.] ------------- MERGE TRACKS ------------- 
+  # Add some features to tracks overall
+  trks <- tracks(list("GWAS"=track.manhattan, "Genes"=track.genes), 
+                 title = title, 
+                 track.bg.color = "transparent",
+                 track.plot.color = "transparent",
+                 label.text.cex = .7, 
+                 label.bg.fill = "grey12",
+                 label.text.color = "white",
+                 label.text.angle = 90) 
+  #
+  # BP_MB <- scales::trans_new("BP_MB", transform = function(x)x/1000000, inverse = function(x)x*1000000)
+  trks <- trks +   
+    scale_x_continuous(name = paste0("BP (",gr.snp$SEQnames[1],")"),
+                       labels=function(x)x/1000000)
+  if(leadSNP.line){
+    trks <- trks +geom_vline(xintercept = lead.snp$POS, 
+                             color="red", alpha=.6, size=.3, linetype='solid') 
+  }
+  # Print
+  if(show_plot) print(trks)
+  # save
+  if(save_plot!=F){
+    print(paste("Saving plot ===>",save_plot))
+    ggsave(filename = save_plot, plot =  trks, dpi=300, height=height, width=width)
+  }
+  return(trks)
+}
